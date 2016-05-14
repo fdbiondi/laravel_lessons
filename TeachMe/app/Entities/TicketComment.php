f2471d8538a549d0b0a49ce2b9e4c56fec86f5d8
fdbiondi <fdbion@gmail.com>
2016-05-14 17:52:00 -0300

    Relate objects with ORM Eloquent

--- TeachMe/app/Entities/TicketComment.php
+++ TeachMe/app/Entities/TicketComment.php
@@ -2,9 +2,15 @@
 
 namespace TeachMe\Entities;
 
-use Illuminate\Database\Eloquent\Model;
-
-class TicketComment extends Model
+class TicketComment extends Entity
 {
-    //
+    public function ticket()
+    {
+        $this->belongsTo(Ticket::getClass());
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::getClass());
+    }
 }
